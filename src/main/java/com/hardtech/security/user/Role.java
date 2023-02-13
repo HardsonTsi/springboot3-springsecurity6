664a5b0b687063cb3c057a5583239f15c9b043c0
TESSI Hardson <hardsontessi2@gmail.com>
2023-02-13 10:12:17 +0100

    initial commit

---
+++ src/main/java/com/hardtech/security/user/Role.java
@@ -0,0 +1,5 @@
+package com.hardtech.security.user;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ src/main/java/com/hardtech/security/user/User.java
@@ -0,0 +1,69 @@
+package com.hardtech.security.user;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity(name = "user_table")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    Integer id;
+    @NotBlank
+    String firstname;
+    @NotBlank
+    String lastname;
+    @NotBlank
+    String email;
+    String password;
+    @Enumerated(EnumType.STRING)
+    Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
